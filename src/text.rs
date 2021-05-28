44e1fe2fff7d412f27f50bac3414e69bde1e4159
Smitty <me@smitop.com>
2021-05-28 12:52:10 -0400

    Better documentation for Text

--- src/text.rs
+++ src/text.rs
@@ -7,6 +7,8 @@ pub struct Lang(pub String);
 /// Text that is in a certain language.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 pub struct Text {
+    /// The raw text.
     pub text: String,
+    /// The language of the text.
     pub lang: Lang,
 }
