dab2888135a81499e1191363a796e48dacaacd20
Smitty <me@smitop.com>
2021-05-28 09:56:13 -0400

    flatten api surface

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 //! Rust library for Wikidata. It has some support for Wikibase as well, although the main focus is
 //! supporting the Wikidata instance.
 
-pub mod entity;
-pub mod ids;
-pub mod text;
+pub(crate) mod entity;
+pub(crate) mod ids;
+pub(crate) mod text;
+
+pub use entity::*;
+pub use ids::*;
+pub use text::*;
