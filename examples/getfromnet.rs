0ba4168b85c72348c49d5fff0c230dcd2c8251d5
Smitty <me@smitop.com>
2021-05-30 17:01:08 -0400

    Add getfromnet example

---
+++ examples/getfromnet.rs
@@ -0,0 +1,16 @@
+use wikidata::*;
+use reqwest;
+
+fn main() {
+    for i in 1_usize.. {
+        let uri = format!("https://www.wikidata.org/wiki/Special:EntityData/Q{}.json", i);
+        let res = reqwest::blocking::get(uri).unwrap();
+        let text = res.text().unwrap();
+        if text.contains("<h1>Not Found</h1><p>No entity with ID ") {
+            continue;
+        }
+        let ent = Entity::from_json(serde_json::from_str(&text).unwrap()).unwrap();
+        let _ = ent;
+        println!("verified Q{}", i);
+    }
+}
