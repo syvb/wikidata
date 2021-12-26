bddc2c0b31ec02df7ec5a8b5e473eeec1c961701
Smitty <me@smitop.com>
2021-12-26 12:30:07 -0500

    Remove some unneeded global clippy allows

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,10 @@
 
 #![warn(clippy::pedantic)]
 #![warn(missing_docs)]
-#![allow(clippy::non_ascii_literal)]
 #![allow(clippy::wildcard_imports)]
 #![allow(clippy::cast_sign_loss)]
 #![allow(clippy::cast_possible_truncation)]
-#![allow(clippy::similar_names)]
 #![allow(clippy::module_name_repetitions)]
-#![allow(clippy::missing_panics_doc)]
 #![allow(clippy::too_many_lines)]
 
 pub(crate) mod entity;
